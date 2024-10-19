7ec58ba1d5e34b58e08423bc49b407f976a6a417
sren94 <stroop94@gmail.com>
2024-10-19 13:42:03 -0600

    Commit v 1.0.6

--- applications/myProfile/models.py
+++ applications/myProfile/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Project(models.Model):
     title = models.CharField('Titulo',max_length=100)
     describe = models.TextField('Descripcion')
-    image = models.ImageField('Portada_De_Proyecto',upload_to=f'{'image'}', default=None)
+    image = models.ImageField('Portada_De_Proyecto', upload_to='image/', default=None)
     url = models.URLField('Link',blank=True)
 
     def __str__(self):
