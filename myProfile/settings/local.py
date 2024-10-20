99e10a23ccb418d0942adddcdf1c3856a250f8f8
sren94 <stroop94@gmail.com>
2024-10-20 10:30:44 -0600

    Commit v 1.1.4

--- myProfile/settings/local.py
+++ myProfile/settings/local.py
@@ -1,77 +1,141 @@
-
 from .base import *
 import os
 import environ
 import dj_database_url
+
+# Cargar las variables de entorno
 env = environ.Env()
 environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
+# Clave secreta y configuración de depuración
 SECRET_KEY = env('SECRET_KEY')
 DEBUG = env.bool('DEBUG', default=False)
 
-#SECRET_KEY = 'django-insecure-lzhsaphvh5#fkds4tfjz(*eoy=mr(of&i%mi9x%u&p-d67jfe='
+# Hosts permitidos
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=[])
 
-#DEBUG = True
+# Orígenes de confianza para CSRF
+CSRF_TRUSTED_ORIGINS = env.list('CSRF_TRUSTED_ORIGINS', default=[])
 
-ALLOWED_HOSTS = [env('ALLOWED_HOSTS')]
-#CSRF_TRUSTED_ORIGINS = [env('CSRF_TRUSTED_ORIGINS')]
+# Configuración de la base de datos
 DATABASES = {
     'default': dj_database_url.config(
         default=os.getenv('DATABASE_URL')
     )
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
-    #     'NAME':env('NAME_DATABASE'),
-    #     'USER':env('DB_USER'),
-    #     'PASSWORD':env('DB_PASSWORD'),
-    #     'HOST':env('HOST'),
-    #     'PORT':'5432',
-    # }
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
-    #     'NAME':env('NAME_DATABASE'),
-    #     'USER':env('DB_USER'),
-    #     'PASSWORD':env('DB_PASSWORD'),
-    #     'HOST':'localhost',
-    #     'PORT':'5432',
-    # }
 }
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.0/howto/static-files/
-STATIC_URL = '/static/'
-#STATICFILES_DIRS=[os.path.join(BASE_DIR, "static")]
-
-#aqui se da registro de los archivos media 
-#y esto sirve para direccionar los recursos
-#se deben de registrar en esta carpeta
-MEDIA_URL='/media/'
-MEDIA_ROOT=os.path.join(BASE_DIR,'media')
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
-# settings.py
-# settings.py
-
-
-# Configuraciones de email
-
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'  # O el servidor de correo que uses
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-#EMAIL_HOST_USER ='stroop94@gmail.com'
-EMAIL_HOST_USER=env('EMAIL_HOST_USER')
-#print(env('EMAIL_HOST_USER'))
-#EMAIL_HOST_PASSWORD ='nsgk uewt qafk fntm'
-EMAIL_HOST_PASSWORD =env('EMAIL_HOST_PASSWORD')
-#print(EMAIL_HOST_PASSWORD)
-
-
+# Configuración de Cloudinary
 CLOUDINARY_STORAGE = {
     'CLOUD_NAME': env('CLOUD_NAME'),
     'API_KEY': env('API_KEY'),
     'API_SECRET': env('API_SECRET'),
 }
+
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
 
+# URLs y rutas para archivos estáticos y media
+STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+# Si tienes archivos estáticos locales, añade esta configuración (opcional)
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+
+# Configuración de email
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = env('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
+
+# Configuración adicional
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Seguridad para producción
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+
+# from .base import *
+# import os
+# import environ
+# import dj_database_url
+# env = environ.Env()
+# environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+
+# SECRET_KEY = env('SECRET_KEY')
+# DEBUG = env.bool('DEBUG', default=False)
+
+# #SECRET_KEY = 'django-insecure-lzhsaphvh5#fkds4tfjz(*eoy=mr(of&i%mi9x%u&p-d67jfe='
+
+# #DEBUG = True
+
+# ALLOWED_HOSTS = [env('ALLOWED_HOSTS')]
+# #CSRF_TRUSTED_ORIGINS = [env('CSRF_TRUSTED_ORIGINS')]
+# DATABASES = {
+#     'default': dj_database_url.config(
+#         default=os.getenv('DATABASE_URL')
+#     )
+#     # 'default': {
+#     #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#     #     'NAME':env('NAME_DATABASE'),
+#     #     'USER':env('DB_USER'),
+#     #     'PASSWORD':env('DB_PASSWORD'),
+#     #     'HOST':env('HOST'),
+#     #     'PORT':'5432',
+#     # }
+#     # 'default': {
+#     #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#     #     'NAME':env('NAME_DATABASE'),
+#     #     'USER':env('DB_USER'),
+#     #     'PASSWORD':env('DB_PASSWORD'),
+#     #     'HOST':'localhost',
+#     #     'PORT':'5432',
+#     # }
+# }
+
+# # Static files (CSS, JavaScript, Images)
+# # https://docs.djangoproject.com/en/5.0/howto/static-files/
+# #STATICFILES_DIRS=[os.path.join(BASE_DIR, "static")]
+
+# #aqui se da registro de los archivos media 
+# #y esto sirve para direccionar los recursos
+# #se deben de registrar en esta carpeta
+# MEDIA_URL='/media/'
+# MEDIA_ROOT=os.path.join(BASE_DIR,'media')
+# # Default primary key field type
+# # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
+# # settings.py
+# # settings.py
+
+
+# # Configuraciones de email
+
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_HOST = 'smtp.gmail.com'  # O el servidor de correo que uses
+# EMAIL_PORT = 587
+# EMAIL_USE_TLS = True
+# #EMAIL_HOST_USER ='stroop94@gmail.com'
+# EMAIL_HOST_USER=env('EMAIL_HOST_USER')
+# #print(env('EMAIL_HOST_USER'))
+# #EMAIL_HOST_PASSWORD ='nsgk uewt qafk fntm'
+# EMAIL_HOST_PASSWORD =env('EMAIL_HOST_PASSWORD')
+# #print(EMAIL_HOST_PASSWORD)
+
+
+# CLOUDINARY_STORAGE = {
+#     'CLOUD_NAME': env('CLOUD_NAME'),
+#     'API_KEY': env('API_KEY'),
+#     'API_SECRET': env('API_SECRET'),
+# }
+# DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+# STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
+
+
+# DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+
+# # Configuración para archivos estáticos si también los subes a Cloudinary
+# STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
+# STATIC_URL = '/static/'
