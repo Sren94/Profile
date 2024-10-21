7f00578ffbc20c4cf267b7ef74b1651272d0ee4c
sren94 <stroop94@gmail.com>
2024-10-20 20:36:23 -0600

    Commit v 1.1.6.9 conf

--- myProfile/urls.py
+++ myProfile/urls.py
@@ -25,3 +25,7 @@ urlpatterns = [
     re_path('success/',include('applications.myProfile.urls'))
 ]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
