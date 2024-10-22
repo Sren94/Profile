41e80c4109dd7d6cfd926cfec36aefafbdf19fc3
sren94 <stroop94@gmail.com>
2024-10-21 19:12:46 -0600

    Commit v 1.1.6.32 conf

--- myProfile/settings/local.py
+++ myProfile/settings/local.py
@@ -26,25 +26,6 @@ DATABASES = {
     )
 
 }
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-        'file': {
-            'level': 'ERROR',
-            'class': 'logging.FileHandler',
-            'filename': os.path.join(BASE_DIR, 'django_error.log'),
-        },
-    },
-    'loggers': {
-        'django': {
-            'handlers': ['file'],
-            'level': 'ERROR',
-            'propagate': True,
-        },
-    },
-}
-
 
 # Configuración de Cloudinary
 cloudinary.config(
@@ -61,8 +42,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 #MEDIA_URL = 'https://res.cloudinary.com/' + env('CLOUD_NAME') + '/media/'
 
-STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
-DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+#STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
+#DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
 # Configuración para almacenamiento específico (opcional, para videos o archivos crudos)
 VIDEO_FILE_STORAGE = 'cloudinary_storage.storage.VideoMediaCloudinaryStorage'
