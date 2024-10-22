4e113e33a34544560266da4578e961baf5c6cb6e
sren94 <stroop94@gmail.com>
2024-10-21 19:31:13 -0600

    Commit v 1.1.6.34 conf

--- myProfile/settings/base.py
+++ myProfile/settings/base.py
@@ -45,7 +45,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'myProfile.urls'
